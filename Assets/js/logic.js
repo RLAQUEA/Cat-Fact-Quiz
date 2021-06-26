a62b18a2e2cb05140bf1d42f78174170bed827e3
rlaquea <rlaquea14@gmail.com>
2021-06-26 16:11:55 -0400

    added timer function

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,1 +1,55 @@
+var time = quizQuestions.length * 15;
+
+var latestQuestionIndex = 0;
+
 var startButton = document.querySelector("#start");
+var questionsFeature = document.querySelector("#questions");
+var timerFeature = document.querySelector("#time");
+var questionChoices = document.querySelector("#options");
+var startScreen = document.querySelector("start-screen");
+var titleFeature = document.querySelector("#question-name")
+var endScreen = document.querySelector("#end-screen")
+var TimerDisplay = document.querySelector(".timer")
+
+function beginQuiz() {
+// startScreen.setAttribute("class", "hidden");
+// questionsFeature.removeAttribute("class");
+timerFunction();
+}
+
+//this is the timer function that counts
+function timerFunction() {
+  var countDown = setInterval(function () {
+        time--;
+        timerFeature.textContent = time;
+
+        if (time == 0) {
+            clearInterval(countDown);
+            //send message to let user know time is up
+        // endQuiz();
+     }
+    }, 1000);
+}
+
+// function endQuiz() {
+//  endScreen.setAttribute("class", "shown");
+// TimerDisplay.removeAttribute("class");
+// }
+
+function getLatestQuestion() {
+    var LatestQuestion = questions[latestQuestionIndex];
+    titleFeature.textContent = latestQuestionIndex;
+    questionChoices.textContent = "This is a string";
+}
+
+for (var i = 0; i < LatestQuestion.choice.length; i++) {
+    var userChoices = document.createElement("button");
+    userChoices.setAttribute("class", "choice");
+    userChoices.setAttribute("value", latestQuestion.choice[i]);
+
+    questionChoices.appendChild(userChoices);
+}
+
+startButton.addEventListener("click", beginQuiz);
+
+
--- Assets/js/quizquestions.js
+++ Assets/js/quizquestions.js
@@ -3,7 +3,19 @@ var quizQuestions = [
     question: "this is my first question", 
     options: ["option1", "option2", "option3"],
     answer: "answer", 
+
+
 },
+
+{
+    question: "this is my first question", 
+    options: ["option1", "option2", "option3"],
+    answer: "answer", 
+
+
+}
 ];
 
-console.log(quizQuestions[0].options[0]); 
+// console.log(quizQuestions[0].options[0]); 
+
+
