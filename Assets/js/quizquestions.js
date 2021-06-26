c6b060c807f6b2de1975ea7a1068e4d45fcf98e3
rlaquea <rlaquea14@gmail.com>
2021-06-25 21:26:42 -0400

    added more starter files, created quiz questions, start button, & initial html classes

---
+++ Assets/js/quizquestions.js
@@ -0,0 +1,9 @@
+var quizQuestions = [
+{
+    question: "this is my first question", 
+    options: ["option1", "option2", "option3"],
+    answer: "answer", 
+},
+];
+
+console.log(quizQuestions[0].options[0]); 
