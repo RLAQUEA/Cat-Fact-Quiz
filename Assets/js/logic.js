00e35f097ec703bccfa11ca082dd4ef0a537a30a
rlaquea <rlaquea14@gmail.com>
2021-06-26 18:43:14 -0400

    Added hide and show feature to start button page to question page

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -6,51 +6,55 @@ var startButton = document.querySelector("#start");
 var questionsFeature = document.querySelector("#questions");
 var timerFeature = document.querySelector("#time");
 var questionChoices = document.querySelector("#options");
-var startScreen = document.querySelector("start-screen");
+var startScreen = document.querySelector("#start-screen");
 var titleFeature = document.querySelector("#question-name")
 var endScreen = document.querySelector("#end-screen")
-var TimerDisplay = document.getElementById(".timer")
 
+
+
+//this function begins the quiz, and first question appears on the page
 function beginQuiz() {
-// startScreen.setAttribute("class", "hidden");
-questionsFeature.removeAttribute("class");
+startScreen.setAttribute("class", "hidden");
+questionsFeature.removeAttribute("class", "hidden");
 timerFunction();
 }
 
-//this is the timer function that counts
+//this is the timer function that counts down from 75 seconds
 function timerFunction() {
   var countDown = setInterval(function () {
         time--;
         timerFeature.textContent = time;
 
         if (time == 0) {
-            clearInterval(countDown);
-            //send message to let user know time is up
-        // endQuiz();
+            clearInterval(countDown);            
+            
+//display message that lets the user know time is up
+endQuiz();
 
         console.log(countDown);
      }
     }, 1000);
 }
 
-// function endQuiz() {
-//  endScreen.setAttribute("class", "shown");
-// TimerDisplay.removeAttribute("class");
-// }
+function getLatestQuestion() {
+var latestQuestion = questions[latestQuestionIndex];
+titleFeature.textContent = latestQuestionIndex;
+questionChoices.textContent = "This is a string";
+console.log(questionChoices);
 
-// function getLatestQuestion() {
-//     var LatestQuestion = questions[latestQuestionIndex];
-//     titleFeature.textContent = latestQuestionIndex;
-//     questionChoices.textContent = "This is a string";
-// }
+for (var i = 0; i < latestQuestion.choice.length; i++) {
+var userChoices = document.createElement("button");
+userChoices.setAttribute("class", "choice");
+userChoices.setAttribute("value", latestQuestion.choice[i]);
+questionChoices.appendChild(userChoices);
+}
 
-// for (var i = 0; i < LatestQuestion.choice.length; i++) {
-//     var userChoices = document.createElement("button");
-//     userChoices.setAttribute("class", "choice");
-//     userChoices.setAttribute("value", latestQuestion.choice[i]);
+function endQuiz() {
+endScreen.setAttribute("class", "shown");
+TimerDisplay.removeAttribute("class", "shown");
+}
 
-//     questionChoices.appendChild(userChoices);
-// }
+}
 
 startButton.addEventListener("click", beginQuiz);
 
